e6ed76cfd4a18e447fcbd238a9842aeca018d872
Fahadi1 <73292833+Fahadi1@users.noreply.github.com>
2021-07-29 10:14:35 +0200

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,4 @@ INSERT INTO `packagingws_db`.`parcel` (`id`, `size`, `weight`) VALUES ('1', 'sma
 INSERT INTO `packagingws_db`.`parcel` (`id`, `size`, `weight`) VALUES ('2', 'medium', '0.25');
 INSERT INTO `packagingws_db`.`parcel` (`id`, `size`, `weight`) VALUES ('3', 'large', '0.5');
 INSERT INTO `packagingws_db`.`parcel` (`id`, `size`, `weight`) VALUES ('4', 'kingsize', '1.0');
+INSERT INTO `packagingws_db`.`colis` (`id`, `final_weight`, `initial_weight`, `parcel_id`) VALUES ('1', '2.4','1.9', '1');
